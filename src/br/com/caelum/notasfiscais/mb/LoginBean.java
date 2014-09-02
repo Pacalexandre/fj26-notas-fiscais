65b9fd86e11c72e8a6334665243b1223bc6227b2
GECOS <jsf4420@caelum-20121024-1.(none)>
2014-09-01 22:49:25 -0300

    tudo

--- src/br/com/caelum/notasfiscais/mb/LoginBean.java
+++ src/br/com/caelum/notasfiscais/mb/LoginBean.java
@@ -1,20 +1,28 @@
 package br.com.caelum.notasfiscais.mb;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 import br.com.caelum.notasfiscais.dao.UsuarioDao;
 import br.com.caelum.notasfiscais.modelo.Usuario;
 
-@ManagedBean
-@SessionScoped
+@Named
+@RequestScoped
 public class LoginBean {
-	private Usuario usuario = new Usuario();
+	
+	@Inject
+	UsuarioDao dao;
+	@Inject
+	UsuarioLogadoBean usuarioLogado;
+	
+	Usuario usuario = new Usuario();
 
 	public String efetuaLogin() {
-		UsuarioDao dao = new UsuarioDao();
+		
 		boolean loginValido = dao.existe(this.usuario);
 		if (loginValido) {
+			usuarioLogado.logar(usuario);
 			return "produto?faces-redirect=true";
 		} else {
 			this.usuario = new Usuario();
