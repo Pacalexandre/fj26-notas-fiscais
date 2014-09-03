189c591ee0f293da61c64ec30db083662b2546e0
GECOS <jsf4420@caelum-20121024-1.(none)>
2014-09-02 21:41:00 -0300

    atual

---
+++ src/br/com/caelum/notasfiscais/mb/NotaFiscalBean.java
@@ -0,0 +1,14 @@
+package br.com.caelum.notasfiscais.mb;
+
+import javax.inject.Named;
+
+import br.com.caelum.notasfiscais.modelo.NotaFiscal;
+
+@Named
+public class NotaFiscalBean {
+
+	
+	NotaFiscal nf = new NotaFiscal();
+	
+	
+}
